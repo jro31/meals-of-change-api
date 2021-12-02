d4a2bc8248c7a7decb876f9f15e338a95001770b
Jethro Williams <jethrowilliams@outlook.com>
2021-12-02 16:30:28 +0000

    Add 'logout' and 'logged_in' actions to the sessions controller
    Including adding a current user concern to set the current user

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "api.mealsofchange.com" # Update this to whatever your backend URL is
+  Rails.application.config.session_store :cookie_store, key: "_meals_of_change", domain: "api.mealsofchange.com" # Update this to whatever your backend URL is
 else
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+  Rails.application.config.session_store :cookie_store, key: "_meals_of_change"
 end
