c84bc843fa4ca2ba2d143bd5858a7ef69ee86eef
Jethro Williams <jethrowilliams@outlook.com>
2021-12-06 10:47:57 +0000

    Move login functionality to api/v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
-  delete :logout, to: 'sessions#logout'
-  get :logged_in, to: 'sessions#logged_in'
+  namespace :api do
+    namespace :v1 do
+      resources :registrations, only: [:create]
+      resources :sessions, only: [:create]
+      get :logged_in, to: 'sessions#logged_in'
+      delete :logout, to: 'sessions#logout'
+    end
+  end
 
   root to: "static#home"
 end
