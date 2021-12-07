8a042e31c50b95e16d3fd6ab28e038737732ff73
Jethro Williams <jethrowilliams@outlook.com>
2021-12-07 14:37:50 +0000

    Add UserRepresenter spec

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class SessionsController < ApplicationController
-      include CurrentUserConcern
+      include CurrentUserConcern # Move this to application controller?
 
       def create
         begin
