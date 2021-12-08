1a226b8ec274fdaea5d71d0e7d0567ecb8238fba
Jethro Williams <jethrowilliams@outlook.com>
2021-12-08 15:56:25 +0000

    Add Tag table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_140340) do
+ActiveRecord::Schema.define(version: 2021_12_08_155051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2021_12_08_140340) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["recipe_id"], name: "index_steps_on_recipe_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Tag, type: :model do
+  subject { create(:tag) }
+  it { expect(subject).to be_valid }
+
+  describe 'validations' do
+    describe 'name' do
+      let(:name) { 'Thai food' }
+      subject { build(:tag, name: name) }
+      it { expect(subject).to be_valid }
+      describe 'validates presence of name' do
+        context 'name is not present' do
+          let(:name) { nil }
+          it 'is invalid with the correct error' do
+            expect(subject).not_to be_valid
+            expect(subject.errors.messages[:name]).to include('can\'t be blank')
+          end
+        end
+      end
+    end
+  end
+end
