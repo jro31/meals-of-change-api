6925f8bd87beb8a2e562af9ffb0a9811137ca5a2
Jethro Williams <jethrowilliams@outlook.com>
2021-12-21 11:05:32 +0000

    Add tags index endpoint (unspecced)

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,8 +1,21 @@
 module Api
   module V1
     class TagsController < Api::V1::BaseController
+      # GET /api/v1/tags
+      # GEt /api/v1/tags?limit=10
       def index
-        # TODO - In order to display the most popular tags in the menu, return the most used tags here
+        begin
+          @tags = policy_scope(Tag).joins(:recipe_tags)
+                                   .group('tags.id')
+                                   .order('count(tags.id) DESC')
+                                   .limit(params[:limit])
+          render json: { tags: @tags.pluck(:name) }, status: :ok
+        rescue => e
+          @skip_after_action = true
+          render json: {
+            error_message: e.message
+          }, status: :not_found
+        end
       end
     end
   end
---
+++ spec/requests/api/v1/tags_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'tags API', type: :request do
+  describe 'GET /api/v1/tags' do
+    # TODO
+  end
+end
