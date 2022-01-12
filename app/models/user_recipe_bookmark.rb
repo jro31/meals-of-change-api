a4486beeaaf9af99f36d3079b38a44d2594c2805
Jethro Williams <jethrowilliams@outlook.com>
2022-01-12 18:19:53 +0000

    Add validation for unique association

--- app/models/user_recipe_bookmark.rb
+++ app/models/user_recipe_bookmark.rb
@@ -2,5 +2,13 @@ class UserRecipeBookmark < ApplicationRecord
   belongs_to :user#, inverse_of: :user_recipe_bookmarks
   belongs_to :recipe#, inverse_of: :user_recipe_bookmarks
 
-  # TODO - Validate uniqueness of assocations
+  validate :validate_unique_association
+
+  private
+
+  def validate_unique_association
+    return unless UserRecipeBookmark.where(user: user, recipe: recipe).where.not(id: id).any?
+
+    errors.add(:user, 'has already favourited this recipe')
+  end
 end
--- spec/models/user_recipe_bookmark_spec.rb
+++ spec/models/user_recipe_bookmark_spec.rb
@@ -17,4 +17,35 @@ describe UserRecipeBookmark, type: :model do
       it { expect(subject.recipe).to eq(recipe) }
     end
   end
+
+  describe 'validations' do
+    describe '#validate_unique_association' do
+      let(:user) { create(:user) }
+      let(:recipe) { create(:recipe) }
+      subject { build(:user_recipe_bookmark, user: user, recipe: recipe) }
+      let(:imposter_user) { create(:user) }
+      let(:imposter_recipe) { create(:recipe) }
+      let!(:imposter_user_recipe_bookmark) { create(:user_recipe_bookmark, user: imposter_user, recipe: imposter_recipe) }
+      it { expect(subject).to be_valid }
+
+      context 'the imposter user recipe bookmark has the same user' do
+        let(:imposter_user) { user }
+        it { expect(subject).to be_valid }
+      end
+
+      context 'the imposter user recipe bookmark has the same recipe' do
+        let(:imposter_recipe) { recipe }
+        it { expect(subject).to be_valid }
+      end
+
+      context 'the imposter user recipe bookmark has the same user and recipe' do
+        let(:imposter_user) { user }
+        let(:imposter_recipe) { recipe }
+        it 'is invalid with the correct error' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.messages[:user]).to include('has already favourited this recipe')
+        end
+      end
+    end
+  end
 end
